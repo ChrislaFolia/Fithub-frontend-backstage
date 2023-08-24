5a0b16880e067217e4bb4bf8cb50ef13a7bbe370
CR4-09\iSpan <frank08ta@gmail.com>
2023-08-24 17:00:50 +0800

    Add tow components courseInsertModal.vue and textModal.vue

---
+++ src/components/courseInsertModal.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="modal fade" id="insertModal" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="insertModalLabel">新增課程資料</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+
+                    <form id="insertCourseForm" method="post" enctype="multipart/form-data" action="#">
+                        <div class="mb-3">
+                            <label for="categoryId" class="col-form-label">課程分類 :</label>
+                            <select class="form-select" name="categoryId" id="categoryId">
+                                <option selected value="" style="display:none">請選擇</option>
+                                <option v-for="{ categoryId, categoryName } in courseCategories" :value=categoryId>
+                                    {{ categoryName }}</option>
+                            </select>
+                        </div>
+
+
+                        <div class="mb-3">
+                            <label for="message-text" class="col-form-label">課程名稱 :</label>
+                            <input type="text" class="form-control" name="courseName" />
+
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="message-text" class="col-form-label">課程圖片 :</label>
+                            <input class="form-control" type="file" name="courseImgFile" id="courseImgFile">
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="message-text" class="col-form-label">課程描述 :</label>
+                            <textarea class="form-control" rows="6" name="courseDescription"></textarea>
+
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" id="sendInsertCourse" @click="sendInsertCourse()"
+                        class="btn btn-primary">送出</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup >
+import { ref } from "vue";
+
+// solution 1
+// const props = defineProps(['courseCategories'])
+
+// solution 2
+const props = defineProps({
+    courseCategories: Object,
+})
+const courses = ref([]);
+// const sendInsertCourse(e){
+//     this.$emit('sendInsertCourse-emit', this.courses)
+// }
+
+</script>
+
+<style scoped></style>
--- src/views/indexCourse.vue
+++ src/views/indexCourse.vue
@@ -9,52 +9,8 @@
                         <button type="button" id="insertCourse" class="btn btn-primary" data-bs-toggle="modal"
                             data-bs-target="#insertModal">新增課程資料</button>
                     </div>
+                    <InsertCourse :courseCategories="courseCategories"></InsertCourse>
 
-                    <div class="modal fade" id="insertModal" tabindex="-1">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="insertModalLabel">新增課程資料</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                        aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-
-                                    <form id="insertCourseForm" method="post" enctype="multipart/form-data"
-                                        action="${pageContext.request.contextPath}/course.controller">
-                                        <div class="mb-3">
-                                            <label for="categoryId" class="col-form-label">課程分類 :</label>
-                                            <select class="form-select" name="categoryId" id="categoryId">
-                                                <option selected value="" style="display:none">請選擇</option>
-                                            </select>
-                                        </div>
-
-
-                                        <div class="mb-3">
-                                            <label for="message-text" class="col-form-label">課程名稱 :</label>
-                                            <input type="text" class="form-control" name="courseName" />
-
-                                        </div>
-
-                                        <div class="mb-3">
-                                            <label for="message-text" class="col-form-label">課程圖片 :</label>
-                                            <input class="form-control" type="file" name="courseImgFile" id="courseImgFile">
-                                        </div>
-
-                                        <div class="mb-3">
-                                            <label for="message-text" class="col-form-label">課程描述 :</label>
-                                            <textarea class="form-control" rows="6" name="courseDescription"></textarea>
-
-                                        </div>
-                                    </form>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="button" id="insertCourseSend" class="btn btn-primary">送出</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
 
                     <div class="card mb-4">
                         <div class="card-header">
@@ -73,7 +29,33 @@
                                         <th>刪除</th>
                                     </tr>
                                 </thead>
-                                <tbody></tbody>
+                                <tbody>
+                                    <tr v-for=" { courseId, courseName, categoryId, courseImgPath, courseDescription }  in   courses  "
+                                        :key="courseId">
+                                        <td>{{ courseName }}</td>
+                                        <td>{{ categoryId }} </td>
+                                        <td>{{ courseImgPath }} </td>
+                                        <td>
+                                            <button type="button" class="btn btn-secondary btn-sm" data-bs-toggle="modal"
+                                                :data-bs-target="'#staticBackdrop' & courseId">
+                                                詳細內容
+                                            </button>
+                                            <courseDescription :parentId="courseId" :parentDescription="courseDescription"
+                                                modalTitle="課程描述">
+                                            </courseDescription>
+                                        </td>
+                                        <td>
+                                            <button id="getUpdateCourseBtn{{courseId}}" class=" btn btn-secondary btn-sm"
+                                                data-bs-toggle="modal" data-bs-target="#updateModal${courseId}">修改
+                                            </button>
+                                        </td>
+                                        <td>
+                                            <button id=" deleteCourse{{course.courseId}}"
+                                                class="btn btn-outline-danger btn-sm">刪除
+                                            </button>
+                                        </td>
+                                    </tr>
+                                </tbody>
                             </table>
                         </div>
                     </div>
@@ -85,7 +67,53 @@
 </template>
     
 <script setup>
+import { ref, reactive } from "vue";
+import axios from "axios";
+import InsertCourse from '../components/courseInsertModal.vue';
+import courseDescription from '../components/textModal.vue';
+const totalPages = ref(0);
+const datas = reactive({
+    start: 0,
+    rows: 0,
+    name: "",
+    sortOrder: "asc",
+    sortType: "id",
+});
 
+// Load course data
+const courses = ref([]);
+const URL = import.meta.env.VITE_API_JAVAURL;
+const loadCourse = async () => {
+    const URLAPI = `${URL}/course/findAll`;
+    const response = await axios.get(URLAPI, datas);
+    // console.log(response.data)
+
+    //取得所有課程放進courses變數
+    courses.value = response.data;
+    console.log(courses)
+    //計算總共幾頁
+    totalPages.value = +datas.rows === 0 ? 1 : Math.ceil(response.data.count / datas.rows)
+};
+
+// Load courseCategories data
+const courseCategories = ref([]);
+const loadCourseCategories = async () => {
+    const URLAPI = `${URL}/coursecategories/findAll`;
+    const response = await axios.get(URLAPI, datas);
+    console.log(response.data)
+
+    //取得所有分類放進courseCategories變數
+    courseCategories.value = response.data;
+    console.log(courseCategories)
+
+};
+
+
+
+
+
+loadCourseCategories();
+loadCourse();
 </script>
     
 <style scoped></style>
