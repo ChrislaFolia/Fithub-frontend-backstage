081183a0552c5587595696bcf50903855e4cae58
CR4-07\iSpan <cc2260112@gmail.com>
2023-08-25 17:13:34 +0800

    新增activity

--- src/views/activity.vue
+++ src/views/activity.vue
@@ -2,47 +2,47 @@
     <div id="layoutSidenav">
         <div id="layoutSidenav_content">
             <div class="container-fluid px-4">
+                <!-- 標題 -->
                 <h1 class="mt-4" style="text-align: center;">全部活動</h1>
                 <div class="card mb-4">
                     <div class="card-body table-responsive">
-
-
+                        <button class="btn btn btn-primary" data-bs-toggle="modal" data-bs-target="#insertModal"
+                            style="margin-bottom: 10px;">新增活動</button>
                         <table class="table table-bordered">
                             <thead class="align-middle text-center">
                                 <tr class="table-primary">
                                     <th>
-                                        <input type="checkbox" id="selectAllCheckbox">
-                                        <button class="btn btn-outline-danger" id="deleteActivitys">刪除</button>
+                                        <button class="btn btn-outline-danger" @click="deleteSelected">刪除</button>
                                     </th>
                                     <th>名稱</th>
-                                    <th>內容</th>
-                                    <th>活動日期</th>
-                                    <th>網址</th>
-                                    <th>顯示</th>
-                                    <th>負責員工</th>
-                                    <th>上架日期</th>
-                                    <th>下架日期</th>
-                                    <th>排序</th>
-                                    <th>圖片</th>
-                                    <th>修改</th>
+									<th>內容</th>
+									<th>活動日期</th>
+									<th>網址</th>
+									<th>顯示</th>
+									<th>負責員工</th>
+									<th>上架日期</th>
+									<th>下架日期</th>
+									<th>排序</th>
+									<th>圖片</th>
+									<th>修改</th>
                                 </tr>
                             </thead>
 
-                            <tbody class="align-middle">
-                                <tr>
-                                    <td>1</td>
-                                    <td>1</td>
-                                    <td>1</td>
-                                    <td>1</td>
-                                    <td>1</td>
-                                    <td>1</td>
-                                    <td>1</td>
-                                    <td>1</td>
-                                    <td>1</td>
-                                    <td>1</td>
-                                    <td class="center"><button class="btn btn-outline-success">預覽</button></td>
-                                    <td class="center"><button type="submit" class="btn btn-outline-info">修改</button></td>
+                            <tbody class="align-middle text-center">
+                                <tr v-for="(Activity, activityindex) in Activitys" :key="activityindex">
+                                    <td><input type="checkbox" v-model="selectedActivities" :value="Activity.activityid"></td> 
+                                    <td>{{ Activity.activityname }}</td>
+                                    <td>{{ Activity.activitydescription }}</td>
+                                    <td>{{ Activity.classroomDescription }}</td>
+                                    <td>{{ Activity.activitydate }}</td>
+                                    <td>{{ Activity.activityurl }}</td>
+                                    <td>{{ Activity.activityurl }}</td>
+                                    <td>{{ Activity.activityurl }}</td>
+                                    <td><img :src="Activity.classroomPic" style="width: 150px;height: 150px;" alt="維修中"></td>
 
+                                    <td><button class="btn btn-outline-secondary" data-bs-toggle="modal"
+                                            @click="openUpdateModal(classroom)" data-bs-target="#updateModal">修改</button>
+                                    </td> 
                                 </tr>
                             </tbody>
                         </table>
@@ -51,8 +51,270 @@
             </div>
         </div>
     </div>
+
+    <!-- 修改-彈出視窗
+    <div class="modal fade" id="updateModal" tabindex="-1" aria-labelledby="updateModal" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">更新教室</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="mb-3">
+                        教室名稱<input v-model="updateSelectedClassroom.classroomName" type="text" class="form-control">
+                        <span v-if="!updateSelectedClassroom.classroomName" class="text-danger">必填</span>
+                    </div>
+                    <div class="mb-3">
+                        容納人數<input v-model="updateSelectedClassroom.classroomCapacity" type="text" class="form-control">
+                        <span v-if="!updateSelectedClassroom.classroomCapacity" class="text-danger">必填</span>
+                    </div>
+                    <div class="mb-3">
+                        設備介紹<input v-model="updateSelectedClassroom.classroomDescription" type="text" class="form-control">
+                        <span v-if="!updateSelectedClassroom.classroomDescription" class="text-danger">必填</span>
+                    </div>
+                    <div class="mb-3">
+                        租借價格<input v-model="updateSelectedClassroom.classroomPrice" type="text" class="form-control">
+                        <span v-if="!updateSelectedClassroom.classroomPrice" class="text-danger">必填</span>
+                    </div>
+                    教室狀態
+                    <div class="mb-3">
+                        <select v-model="updateSelectedClassroom.classroomStatus" class="form-control">
+                            <option value="開放">開放</option>
+                            <option value="關閉">關閉</option>
+                            <option value="維修中">維修中</option>
+                        </select>
+                        <span v-if="!updateSelectedClassroom.classroomStatus" class="text-danger">必填</span>
+                    </div>
+                    <div class="mb-3">
+                        教室圖片:
+                        <input id="updatefile" type="file" class="form-control" accept="image/*" @change="imageUpdate">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary" @click="updateClassroom">送出</button>
+                </div>
+            </div>
+        </div>
+    </div> -->
+
+
+    <!-- 新增-彈出視窗 -->
+    <!-- <div class="modal fade" id="insertModal" tabindex="-1" aria-labelledby="insertModal" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">新增教室</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="mb-3">
+                        教室名稱<input v-model="classroom.classroomName" type="text" class="form-control">
+                        <span v-if="!classroom.classroomName" class="text-danger">必填</span>
+                    </div>
+                    <div class="mb-3">
+                        容納人數<input v-model="classroom.classroomCapacity" type="text" class="form-control">
+                        <span v-if="!classroom.classroomCapacity" class="text-danger">必填</span>
+                    </div>
+                    <div class="mb-3">
+                        設備介紹<input v-model="classroom.classroomDescription" type="text" class="form-control">
+                        <span v-if="!classroom.classroomDescription" class="text-danger">必填</span>
+                    </div>
+                    <div class="mb-3">
+                        租借價格<input v-model="classroom.classroomPrice" type="text" class="form-control">
+                        <span v-if="!classroom.classroomPrice" class="text-danger">必填</span>
+                    </div>
+                    教室狀態
+                    <div class="mb-3">
+                        <select v-model="classroom.classroomStatus" class="form-control">
+                            <option value="開放">開放</option>
+                            <option value="關閉">關閉</option>
+                            <option value="維修中">維修中</option>
+                        </select>
+                        <span v-if="!classroom.classroomStatus" class="text-danger">必填</span>
+                    </div>
+                    <div class="mb-3">
+                        教室圖片:
+                        <input id="insertfile" type="file" class="form-control" accept="image/*" @change="imageInsert">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary" @click="insertClassroom">送出</button>
+                </div>
+            </div>
+        </div>
+    </div> -->
 </template>
-
+  
 <script setup>
+import axios from 'axios'
+import { reactive, ref, onMounted } from 'vue'
 
+//建立教室物件
+const Activity = reactive({
+    classroomName: '',
+    classroomCapacity: '',
+    classroomDescription: '',
+    classroomPrice: '',
+    classroomStatus: '',
+    classroomPic: '',
+});
+
+const Activitys = ref([]); // 儲存SelectAll的活動
+const selectedClassrooms = ref([]); // 儲存選中的 ClassroomID
+const updateSelectedClassroom = reactive({}); // 儲存要修改的教室資料(預設值)
+
+// 将選中的教室資料複製到 updateSelectedClassroom
+const openUpdateModal = (classroom) => {
+    Object.assign(updateSelectedClassroom, classroom);
+
+    //清空上傳圖片欄位
+    let updatefile = document.querySelector('#updatefile')
+    updatefile.value = '';
+};
+
+// 取得圖片轉為BASE64
+const imageInsert = (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        reader.onload = (event) => {
+            const base64Data = event.target.result;
+            classroom.classroomPic = base64Data;
+        };
+    }
+};
+
+// 取得圖片轉為BASE64
+const imageUpdate = (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        reader.onload = (event) => {
+            const base64Data = event.target.result;
+            updateSelectedClassroom.classroomPic = base64Data;
+        };
+    }
+};
+
+
+// 從伺服器獲取 JSON 格式教室資料
+const getActivitys = async () => {
+    try {
+        const response = await axios.get('http://localhost:8080/fithub/activity/list'); // 替換為實際的 API URL
+        Activitys.value = response.data; //data為response物件的屬性，通常是返回的JSON格式資料
+        console.log(Activitys.value)
+
+    } catch (error) {
+        console.error('Error getclassroom data:', error);
+    }
+};
+
+// // 新增教室
+// const insertClassroom = async () => {
+//     try {
+//         // 检查是否有任何必填字段为空
+//         if (!classroom.classroomName ||
+//             !classroom.classroomCapacity ||
+//             !classroom.classroomDescription ||
+//             !classroom.classroomPrice ||
+//             !classroom.classroomStatus) {
+//             return;
+//         }
+
+
+//         const response = await axios.post('http://localhost:8080/fithub/classroom/insert', classroom, {
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             }
+//         });
+
+
+//         //關閉動態框
+//         const insertModal = document.getElementById('insertModal')
+//         let getInstanceInsertModal = bootstrap.Modal.getInstance(insertModal)
+//         getInstanceInsertModal.toggle();
+
+//         // 清空輸入的值
+//         classroom.classroomName = '';
+//         classroom.classroomCapacity = '';
+//         classroom.classroomDescription = '';
+//         classroom.classroomPrice = '';
+//         classroom.classroomStatus = '';
+//         classroom.classroomPic = '';
+//         let insertfile = document.querySelector('#insertfile')
+//         insertfile.value = '';
+
+//         // 刷新畫面
+//         getclassrooms();
+//     } catch (error) {
+//         console.error('Error adding new classroom:', error);
+//     }
+// };
+
+// // 更新教室
+// const updateClassroom = async () => {
+//     try {
+//         // 检查是否有任何必填字段为空
+//         if (!updateSelectedClassroom.classroomName ||
+//             !updateSelectedClassroom.classroomCapacity ||
+//             !updateSelectedClassroom.classroomDescription ||
+//             !updateSelectedClassroom.classroomPrice ||
+//             !updateSelectedClassroom.classroomStatus) {
+//             return;
+//         }
+
+//         const response = await axios.put('http://localhost:8080/fithub/classroom/update', updateSelectedClassroom, {
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             }
+//         });
+
+//         //關閉動態框
+//         const updateModal = document.getElementById('updateModal')
+//         let getInstanceUpdateModal = bootstrap.Modal.getInstance(updateModal)
+//         getInstanceUpdateModal.toggle();
+
+//         // 刷新畫面
+//         getclassrooms();
+//     } catch (error) {
+//         console.error('Error adding new classroom:', error);
+//     }
+// };
+
+// // 刪除多筆教室
+// const deleteSelected = async () => {
+
+//     const checkDelete = window.confirm('確定要刪除選中的教室嗎？');
+//     if (checkDelete) {
+//         try {
+//             // 將選中的 ClassroomID 送到後端進行刪除
+//             const response = await axios.delete('http://localhost:8080/fithub/classroom/delete/multiple', {
+//                 data: selectedClassrooms.value
+//             });
+
+//             // 刷新資料
+//             getclassrooms();
+//             selectedClassrooms.value = []; // 清空選中的項目
+//         } catch (error) {
+//             console.error('Error deleting rent orders:', error);
+//         }
+//     }
+// };
+
+onMounted(() => {
+    getActivitys();
+});
 </script>
+
+<style scoped>
+.text-danger {
+    font-size: 8px;
+}
+</style>
