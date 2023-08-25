b245dcd20b158a017a31d19a8bc429d1f1e1df99
CR4-09\iSpan <frank08ta@gmail.com>
2023-08-25 17:46:28 +0800

    Add LeftNavbar of others

--- src/components/Navbar_Organize.vue
+++ src/components/Navbar_Organize.vue
@@ -1,14 +1,25 @@
 <template>
-	<a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayoutsOrganize"
-		aria-expanded="false" aria-controls="collapseLayouts">
-		<div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-		組織管理
-		<div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-	</a>
-	<div class="collapse" id="collapseLayoutsOrganize" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-		<nav class="sb-sidenav-menu-nested nav">
-			<router-link class="nav-link" to="department">部門分類</router-link>
-			<a class="nav-link" href="employee">員工管理</a>
-		</nav>
-	</div>
+  <a
+    class="nav-link collapsed"
+    href="#"
+    data-bs-toggle="collapse"
+    data-bs-target="#collapseLayoutsOrganize"
+    aria-expanded="false"
+    aria-controls="collapseLayouts"
+  >
+    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+    組織管理
+    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+  </a>
+  <div
+    class="collapse"
+    id="collapseLayoutsOrganize"
+    aria-labelledby="headingOne"
+    data-bs-parent="#sidenavAccordion"
+  >
+    <nav class="sb-sidenav-menu-nested nav">
+      <router-link class="nav-link" to="department">部門分類</router-link>
+      <router-link class="nav-link" to="employee">員工管理</router-link>
+    </nav>
+  </div>
 </template>
