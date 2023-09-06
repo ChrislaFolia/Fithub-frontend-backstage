684eb65e3b055bf7b8b8c67dd0d82cd87c091752
iSpan03 <TW3555488@gmail.com>
2023-09-06 23:20:51 +0800

    新增後台功能 教練照片上傳

--- src/views/coachpic.vue
+++ src/views/coachpic.vue
@@ -1,59 +1,85 @@
 <template>
-    <body class="sb-nav-fixed">
-        <NavbarTop></NavbarTop>
-        <NavbarLeft></NavbarLeft>
+  <body class="sb-nav-fixed">
+    <NavbarTop></NavbarTop>
+    <NavbarLeft></NavbarLeft>
 
-        <div id="layoutSidenav">
-            <div id="layoutSidenav_content">
-                <div class="container-fluid px-4">
-                    <h1 class="mt-4" style="text-align: center;">後台帳戶</h1>
+    <div id="layoutSidenav">
+      <div id="layoutSidenav_content">
+        <div class="container-fluid px-4">
+          <h1 class="mt-4" style="text-align: center">教練照片</h1>
 
-                    <div class="card mb-4">
-                        <div class="card-body table-responsive">
-                            <button type="submit" class="btn btn-outline-info" data-bs-toggle="modal"
-                                data-bs-target="#insertModal">新增帳戶</button>
-
-                            <div class="col-3" style="padding-top: 20px;">
-                                <PageSize @pageSizeChange="changeHandler"></PageSize>
-                            </div>
-                            <div class="col-3">
-                                <SearchTextBox @searchInput="inputHandler"></SearchTextBox>
-                            </div>
-                            <table id="departmentsTable" class="table table-bordered">
-                                <thead class="align-middle text-center">
-                                    <tr class="table-primary">
-                                        <th>教練編號</th>
-                                        <th>教練姓名</th>
-                                        <th>管理照片</th>
-                                    </tr>
-                                </thead>
-                                <tbody class="align-middle text-center">
-                                    <tr v-for="coach in allCoachs" :key="coach.employeeid">
-                                        <td>{{ coach.employeeid }}</td>
-                                        <td>{{ coach.employeename }}</td>
-                                        <td><button type="submit" class="btn btn-outline-info" data-bs-toggle="modal"
-                                                data-bs-target="#updateModal" @click="inputUpdateData(coach)">修改</button>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                            <Paging :totalPages="totalPages" :thePage="datas.start + 1" @abcClick="clickHandler"></Paging>
-                        </div>
-                    </div>
-                </div>
+          <div class="card mb-4">
+            <div class="card-body table-responsive">
+              <div class="col-3" style="padding-top: 20px">
+                <PageSize @pageSizeChange="changeHandler"></PageSize>
+              </div>
+              <div class="col-3">
+                <SearchTextBox @searchInput="inputHandler"></SearchTextBox>
+              </div>
+              <table id="departmentsTable" class="table table-bordered">
+                <thead class="align-middle text-center">
+                  <tr class="table-primary">
+                    <th>教練編號</th>
+                    <th>教練姓名</th>
+                    <th>管理照片</th>
+                  </tr>
+                </thead>
+                <tbody class="align-middle text-center">
+                  <tr v-for="coach in allCoachs" :key="coach.employeeid">
+                    <td>{{ coach.employeeid }}</td>
+                    <td>{{ coach.employeename }}</td>
+                    <td>
+                      <button
+                        type="submit"
+                        class="btn btn-outline-info"
+                        data-bs-toggle="modal"
+                        data-bs-target="#updateModal"
+                        @click="inputUpdateData(coach)"
+                      >
+                        修改
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <Paging
+                :totalPages="totalPages"
+                :thePage="datas.start + 1"
+                @abcClick="clickHandler"
+              ></Paging>
             </div>
+          </div>
         </div>
+      </div>
+    </div>
 
-        <!-- <div class="modal fade" id="updateModal" tabindex="-1" aria-labelledby="updateModal" aria-hidden="true"
-            data-bs-backdrop="static">
-            <div class="modal-dialog modal-dialog-scrollable">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">修改後台帳戶</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="mb-3">
+    <div
+      class="modal fade"
+      id="updateModal"
+      tabindex="-1"
+      aria-labelledby="updateModal"
+      aria-hidden="true"
+      data-bs-backdrop="static"
+    >
+      <div class="modal-dialog modal-dialog-scrollable">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">修改教練照片</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <img
+              style="width: 200px"
+              v-for="cpic in updateCoachPic"
+              :src="`data:image/jpeg;base64,${cpic.cpicfile}`"
+            />
+            <input id="inputImg" type="file" multiple />
+            <!-- <div class="mb-3">
                             員工編號: {{ updateBackStageAccount.cpicid }}
                         </div>
                         <div class="mb-3">
@@ -84,13 +110,12 @@
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">取消</button>
                         <button type="submit" class="btn btn-primary" @click="updateData()">修改</button>
-                    </div>
-
-                </div>
-            </div>
+                    </div> -->
+          </div>
         </div>
+      </div>
 
-        <div class="modal fade" id="insertModal" tabindex="-1" aria-labelledby="insertModal" aria-hidden="true"
+      <!-- <div class="modal fade" id="insertModal" tabindex="-1" aria-labelledby="insertModal" aria-hidden="true"
             data-bs-backdrop="static">
             <div class="modal-dialog modal-dialog-scrollable">
                 <div class="modal-content">
@@ -162,103 +187,146 @@
                         <button type="submit" class="btn btn-primary" @click="deleteData()">刪除</button>
                     </div>
                 </div>
-            </div>
-        </div> -->
-    </body>
+            </div>-->
+    </div>
+  </body>
 </template>
 
 <script setup>
-import axios from 'axios';
-import { ref, reactive, onMounted } from 'vue'
-import NavbarTop from '../components/NavbarTop.vue'
-import NavbarLeft from '../components/NavbarLeft.vue'
+import axios from "axios";
+import { ref, reactive, onMounted } from "vue";
+import NavbarTop from "../components/NavbarTop.vue";
+import NavbarLeft from "../components/NavbarLeft.vue";
 import Paging from "../components/Paging.vue";
 import PageSize from "../components/PageSize.vue";
-import SearchTextBox from '../components/SearchTextBox.vue'
+import SearchTextBox from "../components/SearchTextBox.vue";
 
-const url = import.meta.env.VITE_API_JAVAURL
+const url = import.meta.env.VITE_API_JAVAURL;
 const insertCoachPic = reactive({
-    cpicid: '',
-    employeeid: '',
-    cpicfile: '',
+  cpicid: "",
+  employeeid: "",
+  cpicfile: "",
 });
-
-
 
 const updateCoachPic = reactive({});
 const deleteBackStageAccount = reactive({});
 
-const allCoachs = ref([])
+const allCoachs = ref([]);
+
+const updateEmp = ref([]);
 
 const totalPages = ref(0);
 const datas = reactive({
-    start: 0,
-    rows: 5,
-    name: null,
-    sortOrder: "asc",
-    sortType: "id",
+  start: 0,
+  rows: 5,
+  name: null,
+  sortOrder: "asc",
+  sortType: "id",
 });
 
+const selectedFile = ref(null);
+
 const loadDatas = async () => {
-    const responseCoachs = await axios.post(`${url}/employees/findCoachPageByName`, datas)
+  const responseCoachs = await axios.post(`${url}/employees/findCoachPageByName`, datas);
 
-    allCoachs.value = responseCoachs.data.list
+  allCoachs.value = responseCoachs.data.list;
 
-    // 計算總共幾頁
-    totalPages.value = +datas.rows === 0 ? 1 : Math.ceil(responseCoachs.data.count / datas.rows)
-
-}
+  // 計算總共幾頁
+  totalPages.value =
+    +datas.rows === 0 ? 1 : Math.ceil(responseCoachs.data.count / datas.rows);
+};
 
 onMounted(() => {
-    loadDatas()
+  loadDatas();
+
+  var inputImg = document.getElementById("inputImg");
+
+  inputImg.addEventListener("change", async function (e) {
+    selectedFile.value = e.target.files;
+    // console.log(updateEmp.value)
+    // console.log(updateCoachPic[0].employeeid)
+    try {
+      await uploadImage()
+      
+    } catch (error) {}
+  });
 });
 
 //paging 由子元件觸發
-const clickHandler = page => {
-    datas.start = page - 1
-    loadDatas()
-}
+const clickHandler = (page) => {
+  datas.start = page - 1;
+  loadDatas();
+};
 
 //一頁幾筆資料
-const changeHandler = value => {
-    datas.rows = value
-    datas.start = 0
-    console.log("pagesize：", datas)
-    loadDatas()
-}
+const changeHandler = (value) => {
+  datas.rows = value;
+  datas.start = 0;
+  console.log("pagesize：", datas);
+  loadDatas();
+};
 
 //排序
-const sortHandler = type => {
-    datas.sortOrder = datas.sortOrder === "asc" ? "desc" : "asc"
-    datas.sortType = type
-    loadDatas()
-}
+const sortHandler = (type) => {
+  datas.sortOrder = datas.sortOrder === "asc" ? "desc" : "asc";
+  datas.sortType = type;
+  loadDatas();
+};
 
 //搜尋
-const inputHandler = value => {
-    datas.name = value
-    datas.start = 0
-    loadDatas()
-}
-
+const inputHandler = (value) => {
+  datas.name = value;
+  datas.start = 0;
+  loadDatas();
+};
 
 // 點擊修改時觸發 帶入該筆資料
 const inputUpdateData = async (emp) => {
-    Object.assign(updateCoachPic, emp);
+  console.log("inputUpdateData");
+  for (const key in updateCoachPic) {
+    delete updateCoachPic[key];
+  }
+  updateEmp.value = emp;
+  const responseCoachPics = await axios.get(`${url}/coachpics/byemp/${emp.employeeid}`);
+  Object.assign(updateCoachPic, responseCoachPics.data);
 
-    const responseCoachPics = await axios.get(`${url}/coachpics/byemp/${emp.employeeid}`)
-    
-    console.log(responseCoachPics)
-    console.log(responseCoachPics.data)
+  console.log("updateCoachPic");
+  console.log(updateCoachPic);
+};
+
+const uploadImage = async () => {
+  console.log("uploadImage");
+
+  if (!selectedFile.value || selectedFile.value.length === 0) {
+    return;
+  }
+
+  for (let i = 0; i < selectedFile.value.length; i++) {
+    const reader = new FileReader();
+    reader.onload = async (event) => {
+      const base64Image = event.target.result;
+      const jsonPayload = {
+        employeeid: updateEmp.value.employeeid,
+        cpicfile: base64Image,
+      };
+
+      try {
+        const response = await axios.post(`${url}/coachpics`, jsonPayload);
+        console.log(response.status)
+        inputUpdateData(updateEmp.value)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    reader.readAsDataURL(selectedFile.value[i]);
+  }
 };
 
 // // 點擊刪除時觸發 帶入該筆資料
 // const inputDeleteData = (data) => {
 //     Object.assign(deleteBackStageAccount, data);
 // };
-
-
-
 
 // const insertData = async () => {
 //     // //抓彈出視窗
@@ -332,7 +400,6 @@ const inputUpdateData = async (emp) => {
 //             alert("修改成功")
 //         }
 
-
 //     } catch (error) {
 //         console.log(error.response)
 //         alert("修改失敗")
@@ -342,7 +409,6 @@ const inputUpdateData = async (emp) => {
 //         cleardata();
 //     }
 // }
-
 
 // const deleteData = async () => {
 //     //抓彈出視窗
@@ -361,7 +427,6 @@ const inputUpdateData = async (emp) => {
 //             alert("刪除成功")
 //         }
 
-
 //     } catch (error) {
 //         console.log(error.response)
 //         alert("刪除失敗")
@@ -371,5 +436,4 @@ const inputUpdateData = async (emp) => {
 //     }
 
 // }
-
 </script>
