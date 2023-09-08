9ccf3ba18d23213750d556eb27ae1aa57f3b5e22
CR4-09\iSpan <frank08ta@gmail.com>
2023-09-08 18:32:34 +0800

    fix conflict of NavbarLeft

--- src/components/NavbarLeft.vue
+++ src/components/NavbarLeft.vue
@@ -1,64 +1,56 @@
 <script setup>
-import { ref , onMounted} from 'vue';
-import Activity from './Navbar_Acitivity.vue'
-import Organize from './Navbar_Organize.vue'
-import Course from './Navbar_Course.vue'
-<<<<<<< HEAD
-import Announcement from './Navbar_Announcement.vue'
-import RentOrder from './Navbar_RentOrder.vue'
-import Classroom from './Navbar_Classroom.vue'
-import TestNavbar from './NavbarTest.vue'
-import Member from './Navbar_member.vue'
-const selectedOption = ref('both'); // 預設選擇單一 Activity 組件
-=======
-import RentOrder from './Navbar_RentOrder.vue';
-import Classroom from './Navbar_Classroom.vue';
-import Announcement from './Navbar_Announcement.vue';
->>>>>>> course
+import { ref, onMounted } from "vue";
+import Activity from "./Navbar_Acitivity.vue";
+import Organize from "./Navbar_Organize.vue";
+import Course from "./Navbar_Course.vue";
+import Announcement from "./Navbar_Announcement.vue";
+import RentOrder from "./Navbar_RentOrder.vue";
+import Classroom from "./Navbar_Classroom.vue";
+import TestNavbar from "./NavbarTest.vue";
+import Member from "./Navbar_member.vue";
+const selectedOption = ref("both"); // 預設選擇單一 Activity 組件
 
 onMounted(() => {
-    selectedOption.value = window.localStorage.getItem("loa");
-    console.log(selectedOption.value)
-    console.log(typeof selectedOption.value)
+  selectedOption.value = window.localStorage.getItem("loa");
+  console.log(selectedOption.value);
+  console.log(typeof selectedOption.value);
 });
-
 </script>
 
 <template>
-    <div id="layoutSidenav">
-        <div id="layoutSidenav_nav">
-            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                <div class="sb-sidenav-menu">
-                    <div class="nav">
-<<<<<<< HEAD
-                        <div v-if="selectedOption === '3'">
-                            <Announcement></Announcement>
-                            <Activity></Activity>
-                            <Course></Course>
-                            <RentOrder></RentOrder>
-                            <Classroom></Classroom>
-                            <Member></Member>
-                            <Organize></Organize>
-                            <TestNavbar></TestNavbar>                   
-                        </div>
-                        <div v-else-if="selectedOption === '2'">
-                            <Announcement></Announcement>
-                            <Activity></Activity>
-                            <Course></Course>
-                            <RentOrder></RentOrder>
-                            <Classroom></Classroom>
-                            <Member></Member>
-                        </div>
-                        <div v-else-if="selectedOption === '1'">
-                            <Announcement></Announcement>
-                            <Activity></Activity>
-                            <Course></Course>
-                            <RentOrder></RentOrder>
-                            <Classroom></Classroom>
-                        </div>
-                       
-                        <!-- 條件渲染動態組件 -->
-                        <!-- <div v-if="selectedOption === 'singleActivity'">
+  <div id="layoutSidenav">
+    <div id="layoutSidenav_nav">
+      <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+        <div class="sb-sidenav-menu">
+          <div class="nav">
+            <div v-if="selectedOption === '3'">
+              <Announcement></Announcement>
+              <Activity></Activity>
+              <Course></Course>
+              <RentOrder></RentOrder>
+              <Classroom></Classroom>
+              <Member></Member>
+              <Organize></Organize>
+              <TestNavbar></TestNavbar>
+            </div>
+            <div v-else-if="selectedOption === '2'">
+              <Announcement></Announcement>
+              <Activity></Activity>
+              <Course></Course>
+              <RentOrder></RentOrder>
+              <Classroom></Classroom>
+              <Member></Member>
+            </div>
+            <div v-else-if="selectedOption === '1'">
+              <Announcement></Announcement>
+              <Activity></Activity>
+              <Course></Course>
+              <RentOrder></RentOrder>
+              <Classroom></Classroom>
+            </div>
+
+            <!-- 條件渲染動態組件 -->
+            <!-- <div v-if="selectedOption === 'singleActivity'">
                             <Activity></Activity>
                         </div>
                         <div v-else-if="selectedOption === 'singleOrganize'">
@@ -74,18 +66,9 @@ onMounted(() => {
                         <div v-else-if="selectedOption === 'both'">
                           
                         </div> -->
-=======
-                        <Activity></Activity>
-                        <Organize></Organize>
-                        <Course></Course>
-                        <Classroom></Classroom>
-                        <RentOrder></RentOrder>
-                        <Announcement></Announcement>
->>>>>>> course
-                    </div>
-                </div>
-            </nav>
+          </div>
         </div>
+      </nav>
     </div>
+  </div>
 </template>
-
