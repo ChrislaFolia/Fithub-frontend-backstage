7d287f1a0a7090ccf5f70650ed567d8420f66996
CR4-09\iSpan <frank08ta@gmail.com>
2023-09-08 18:49:00 +0800

    fix navbar component import on order and coupon related components

--- src/views/orderitem.vue
+++ src/views/orderitem.vue
@@ -1,55 +1,59 @@
 <template>
+  <body class="sb-nav-fixed">
+    <NavbarTop></NavbarTop>
+    <NavbarLeft></NavbarLeft>
     <div id="layoutSidenav">
-        <div id="layoutSidenav_content">
-            <div class="container-fluid px-4">
-                <h1 class="mt-4" style="text-align: center;">訂單項目</h1>
-                <div class="card mb-4">
-                    <div class="card-body table-responsive">
-                        <table class="table table-bordered">
-                            <thead class="align-middle text-center">
-                                <tr class="table-primary">
-                                    <th>訂單編號</th>
-                                    <th>課程名稱</th>
-                                    <th>課程日期/時間</th>
-                                    <th>上課教室</th>
-                                    <th>課程價格</th>
-                                    <th>優惠金額</th>
-                                </tr>
-                            </thead>
+      <div id="layoutSidenav_content">
+        <div class="container-fluid px-4">
+          <h1 class="mt-4" style="text-align: center">訂單項目</h1>
+          <div class="card mb-4">
+            <div class="card-body table-responsive">
+              <table class="table table-bordered">
+                <thead class="align-middle text-center">
+                  <tr class="table-primary">
+                    <th>訂單編號</th>
+                    <th>課程名稱</th>
+                    <th>課程日期/時間</th>
+                    <th>上課教室</th>
+                    <th>課程價格</th>
+                    <th>優惠金額</th>
+                  </tr>
+                </thead>
 
-                            <tbody class="align-middle text-center">
-                                <tr v-for="(orderitem, orderitemindex) in orderitems" :key="orderitemindex">
-                                    <td>{{ orderitem.orderId }}</td>
-                                     <td>{{ orderitem.classes.course.courseName }}</td>
-                                     <td>{{ orderitem.classes.classDate }}
-                                        {{ orderitem.classes.classTime }}
-                                    </td>
-                                    <td>{{ orderitem.classes.classroom.classroomName }}</td>
-                                    <td>{{ orderitem.classes.price }}</td>
-                                    <td>{{ orderitem.coupon.coupondiscount }}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                <tbody class="align-middle text-center">
+                  <tr
+                    v-for="(orderitem, orderitemindex) in orderitems"
+                    :key="orderitemindex"
+                  >
+                    <td>{{ orderitem.orderId }}</td>
+                    <td>{{ orderitem.classes.course.courseName }}</td>
+                    <td>
+                      {{ orderitem.classes.classDate }}
+                      {{ orderitem.classes.classTime }}
+                    </td>
+                    <td>{{ orderitem.classes.classroom.classroomName }}</td>
+                    <td>{{ orderitem.classes.price }}</td>
+                    <td>{{ orderitem.coupon.coupondiscount }}</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
+          </div>
         </div>
+      </div>
     </div>
+  </body>
 </template>
 <script setup>
-import axios from 'axios'
-import { reactive, ref, onMounted } from 'vue'
-
-
-
+import axios from "axios";
+import { reactive, ref, onMounted } from "vue";
 
 //建立優惠券物件
-const orderitem = reactive(
-    {
-        itemid: '',
-        orderdid: '',
-        classid: '',
-        couponid: '',
+const orderitem = reactive({
+  itemid: "",
+  orderdid: "",
+  classid: "",
+  couponid: "",
 });
 // const translateOrderState = (state) => {
 //   const stateTranslations = {
@@ -64,20 +68,17 @@ const orderitem = reactive(
 const orderitems = ref([]); // 儲存SelectAll的訂單
 const selectedOrderss = ref([]); // 儲存選中的 ClassroomID
 
-
-
-
-
 // 從伺服器獲取 JSON 格式優惠券資料
 const getorderitems = async () => {
-    try {
-        const response = await axios.get('http://localhost:8080/fithub/order-items'); // 替換為實際的 API URL
-        orderitems.value = response.data; //data為response物件的屬性，通常是返回的JSON格式資料
-        console.log(orderitems.value)
-
-    } catch (error) {
-        console.error('Error getorderss data:', error);
-    }
+  try {
+    const response = await axios.get(
+      "http://localhost:8080/fithub/order-items"
+    ); // 替換為實際的 API URL
+    orderitems.value = response.data; //data為response物件的屬性，通常是返回的JSON格式資料
+    console.log(orderitems.value);
+  } catch (error) {
+    console.error("Error getorderss data:", error);
+  }
 };
 
 // const getCouponCategoryName = (coupon) => {
@@ -87,16 +88,13 @@ const getorderitems = async () => {
 //       return '未知類別';
 //     };
 
-
-
 onMounted(() => {
-    getorderitems();
-    
+  getorderitems();
 });
 </script>
 
 <style scoped>
 .text-danger {
-    font-size: 8px;
+  font-size: 8px;
 }
 </style>
