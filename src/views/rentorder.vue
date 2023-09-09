b567322838b46a6b570c56c03150f21543bcef6d
william <cc2260112@gmail.com>
2023-09-09 13:13:30 +0800

    新增租借訂單>匯出訂單

--- src/views/rentorder.vue
+++ src/views/rentorder.vue
@@ -8,7 +8,10 @@
                     <h1 class="mt-4" style="text-align: center;">場地租借</h1>
                     <div class="card mb-4">
                         <div class="card-body table-responsive">
-                            <table class="table table-bordered">
+                            <button class="btn btn btn-primary" @click="exportXlsx" style="margin-bottom: 10px">
+                                匯出訂單
+                            </button>
+                            <table id="table1" class="table table-bordered">
                                 <thead class="align-middle text-center">
                                     <tr class="table-primary">
                                         <th>
@@ -59,21 +62,20 @@
                             租借場地:<input type="text" class="form-control" required="required">
                         </div>
                         <div class="mb-3">
-                            日期:<input type="date" class="form-control" required="required">
+                            預約日期:<input type="date" class="form-control" required="required">
                         </div>
                         <div class="mb-3">
                             時段:<select class="form-control">
                                 <option>早上</option>
-                                <option>中午</option>
                                 <option>下午</option>
+                                <option>晚上</option>
                             </select>
                         </div>
                         <div class="mb-3">
                             租借狀態:<select class="form-control">
-                                <option>處理中</option>
-                                <option>已確認</option>
-                                <option>已完成</option>
-                                <option>已取消</option>
+                                <option>未付款</option>
+                                <option>已付款</option>
+                                <option>取消</option>
                             </select>
                         </div>
                     </div>
@@ -92,15 +94,36 @@ import axios from 'axios'
 import { ref, onMounted } from 'vue'
 import NavbarTop from '../components/NavbarTop.vue'
 import NavbarLeft from '../components/NavbarLeft.vue'
+import * as XLSX from 'xlsx'
 
 const rentOrders = ref([]); // 使用 ref 創建一個響應式變數
 const selectedRentOrderIds = ref([]); // 儲存選中的 rentorderid
+const xlsxData = ref([]) //儲存匯出xlsx需要的資料
+
+// 將json資料匯出xlsx檔
+function exportXlsx() {
+    const wb = XLSX.utils.book_new()
+    const ws = XLSX.utils.json_to_sheet(xlsxData.value)
+    XLSX.utils.book_append_sheet(wb, ws, '租借訂單')
+    XLSX.writeFile(wb, 'RentOrder.xlsx')
+}
 
 // 從伺服器獲取 JSON 資料
 const getrentorder = async () => {
     try {
         const response = await axios.get('http://localhost:8080/fithub/rent/list'); // 替換為實際的 API URL
         rentOrders.value = response.data; //data為response物件的屬性，通常是返回的JSON格式資料
+
+        xlsxData.value = rentOrders.value.map(rentOrder => ({
+            rentOrderId: rentOrder.rentorderid,
+            rentOrderDate: rentOrder.rentorderdate,
+            memberName: rentOrder.member.membername,
+            classroomName: rentOrder.classroom.classroomName,
+            rentDate: rentOrder.rentdate,
+            rentTime: rentOrder.renttime,
+            rentStatus: rentOrder.rentstatus,
+        }));
+
         console.log(rentOrders.value)
 
     } catch (error) {
@@ -108,7 +131,7 @@ const getrentorder = async () => {
     }
 };
 
-
+// 刪除多筆教室
 const deleteSelected = async () => {
 
     const checkDelete = window.confirm('確定要刪除選中的教室嗎？');
